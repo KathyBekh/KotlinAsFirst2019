8ba008be4e9fc5b81fef4f6930ff0692a85ed9c6
Katerina Bekhtold <kbehtold@yandex.ru>
2019-09-13 23:26:53 +0300

    Implement lesson 1 tasks

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -116,7 +116,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-    fun accountInThreeYears(initial: Int, percent: Int): Double {
+fun accountInThreeYears(initial: Int, percent: Int): Double {
     fun accountProfit(profit: Double): Double =
         profit + (profit * percent / 100.0)
 
